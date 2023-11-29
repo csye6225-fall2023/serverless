f43d769be59162b10860d982607185d75ca17d68
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-28 21:12:01 -0500

    email and attribute update

--- functions/storeToDynamo.js
+++ functions/storeToDynamo.js
@@ -2,8 +2,7 @@ const AWS = require('aws-sdk');
 
 const storeToDynamo = async (email, status) => {
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
-    let res = null;
-
+  
     try {
         //send data to dynamodb
         const params = {
@@ -17,13 +16,9 @@ const storeToDynamo = async (email, status) => {
         };
             
         await dynamoDB.put(params).promise();
-        res = 'success';
     } catch(e) {
         console.log(e);
-        res = 'failure';
     }
-
-    return res;
 }
 
 module.exports = storeToDynamo;
--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const sendMail = require('./functions/mailUser');
 const storeToDynamo = require('./functions/storeToDynamo');
 
 exports.handler = async (event) => {
+  let response = null;
+  
   try {    
-    let response = null;
   
     //get data from event
     const message = JSON.parse(event.Records[0].Sns.Message);
-    const {email, submission_url, assignment_id} = message;
+    const {email, submission_url, assignment_id, account_id} = message;
   
     //store to bucket
-    let status = await storeToBucket(submission_url, assignment_id);
+    const status = await storeToBucket(submission_url, assignment_id, account_id);
   
     //send mail
-    status = await sendMail(email, status);
-  
-    //send data to dynamodb
-    status = await storeToDynamo(email, status);
+    await sendMail(email, status);
 
+    //send data to dynamodb
+    await storeToDynamo(email, status === 'success' ? 'success' : 'failed');
+  
     response = {
       statusCode: 200,
       body: "success",
